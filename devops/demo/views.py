fd74636b92402b4401bd33b4327cb03665e3f76b
Satyach23 <chsatyasravani23@gmail.com>
2025-08-08 18:07:53 -0400

    Update views.py

--- devops/demo/views.py
+++ devops/demo/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
 
-# Create your views here.
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>Welcome to the Demo App!</h1><p>This is a basic Django app.</p>")
+    return HttpResponse("Hello! This is my first Django page.")
+
 
