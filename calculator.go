7f2fdad4c115c04b13faab1ad949893fa7f130fe
Satyach23 <chsatyasravani23@gmail.com>
2025-08-11 16:48:31 -0400

    simple calculator app

--- calculator.go
+++ calculator.go
@@ -12,54 +12,41 @@ func main() {
 	fmt.Println("Hi Satya Chintala, I am a calculator app ....")
 
 	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
+	text = strings.TrimSpace(text)
 
-	for {
-		fmt.Print("Enter any calculation (Example: 1 + 2 (or) 2 * 5 -> Please maintain spaces as shown in example): ")
-		text, _ := reader.ReadString('\n')
-		text = strings.TrimSpace(text)
-
-		if text == "exit" {
-			break
-		}
-
-		parts := strings.Split(text, " ")
-		if len(parts) != 3 {
-			fmt.Println("Invalid input. Try again.")
-			continue
-		}
-
-		left, err := strconv.Atoi(parts[0])
-		if err != nil {
-			fmt.Println("Invalid input. Try again.")
-			continue
-		}
-
-		right, err := strconv.Atoi(parts[2])
-		if err != nil {
-			fmt.Println("Invalid input. Try again.")
-			continue
-		}
-
-		var result int
-		switch parts[1] {
-		case "+":
-			result = left + right
-		case "-":
-			result = left - right
-		case "*":
-			result = left * right
-		case "/":
-			if right == 0 {
-				fmt.Println("Error: Division by zero.")
-				continue
-			}
-			result = left / right
-		default:
-			fmt.Println("Invalid operator. Try again.")
-			continue
-		}
-
-		fmt.Println("Result:", result)
+	parts := strings.Split(text, " ")
+	if len(parts) != 3 {
+		fmt.Println("Invalid input.")
+		return
 	}
+
+	left, err1 := strconv.Atoi(parts[0])
+	right, err2 := strconv.Atoi(parts[2])
+	if err1 != nil || err2 != nil {
+		fmt.Println("Invalid numbers.")
+		return
+	}
+
+	var result int
+	switch parts[1] {
+	case "+":
+		result = left + right
+	case "-":
+		result = left - right
+	case "*":
+		result = left * right
+	case "/":
+		if right == 0 {
+			fmt.Println("Error: Division by zero.")
+			return
+		}
+		result = left / right
+	default:
+		fmt.Println("Invalid operator.")
+		return
+	}
+
+	fmt.Println("Result:", result)
 }
 
