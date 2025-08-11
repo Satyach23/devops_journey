d16a8a07d6d81a7d5d2c1bca07ab905b85c52b73
Satyach23 <chsatyasravani23@gmail.com>
2025-08-11 16:07:35 -0400

    Updated calculator app

--- calculator.go
+++ calculator.go
@@ -9,42 +9,37 @@ import (
 )
 
 func main() {
-	fmt.Println("Hi Abhishek.Veeramalla, I am a calculator app ....")
+	fmt.Println("Hi Satya Chintala, I am a calculator app ....")
+
+	reader := bufio.NewReader(os.Stdin)
 
 	for {
-		// Read input from the user
-		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("Enter any calculation (Example: 1 + 2 (or) 2 * 5 -> Please maintain spaces as shown in example): ")
 		text, _ := reader.ReadString('\n')
-
-		// Trim the newline character from the input
 		text = strings.TrimSpace(text)
 
-		// Check if the user entered "exit" to quit the program
 		if text == "exit" {
 			break
 		}
 
-		// Split the input into two parts: the left operand and the right operand
 		parts := strings.Split(text, " ")
 		if len(parts) != 3 {
 			fmt.Println("Invalid input. Try again.")
 			continue
 		}
 
-		// Convert the operands to integers
 		left, err := strconv.Atoi(parts[0])
 		if err != nil {
 			fmt.Println("Invalid input. Try again.")
 			continue
 		}
+
 		right, err := strconv.Atoi(parts[2])
 		if err != nil {
 			fmt.Println("Invalid input. Try again.")
 			continue
 		}
 
-		// Perform the calculation based on the operator
 		var result int
 		switch parts[1] {
 		case "+":
@@ -54,13 +49,17 @@ func main() {
 		case "*":
 			result = left * right
 		case "/":
+			if right == 0 {
+				fmt.Println("Error: Division by zero.")
+				continue
+			}
 			result = left / right
 		default:
 			fmt.Println("Invalid operator. Try again.")
 			continue
 		}
 
-		// Print the result
-		fmt.Printf("Result: %d\n", result)
+		fmt.Println("Result:", result)
 	}
 }
+
